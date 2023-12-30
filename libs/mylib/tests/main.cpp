ff53508ba345a6496fe1aceaf56efd1eff5ba491
gorec323 <sgorcev84@mail.ru>
2023-12-30 10:39:39 +0300

    Initial commit

---
+++ libs/mylib/tests/main.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include <mylib.hpp>
+
+TEST(MylibTest, AnyTest)
+{
+    EXPECT_EQ(true, false)
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "version.h"
+
+using namespace std;
+
+int main()
+{
+    using namespace std;
+
+    cout << "My app version: " << PROJECT_VERSION << endl;
+
+    return 0;
+}
